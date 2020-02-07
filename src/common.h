ca9b0ba7ec814ce4c55c71b0ca85239795c872fa
Martin Endler <pokusew@seznam.cz>
2020-02-07 23:31:13 +0100

    Fix compilation warnings on Windows

--- src/common.h
+++ src/common.h
@@ -19,7 +19,7 @@ namespace {
     std::string error_msg(const char* method, LONG result) {
         char msg[ERR_MSG_MAX_LEN];
 #ifdef _WIN32
-        LPVOID lpMsgBuf;
+        LPTSTR lpMsgBuf;
         FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER |
                        FORMAT_MESSAGE_FROM_SYSTEM |
                        FORMAT_MESSAGE_IGNORE_INSERTS,
@@ -43,7 +43,7 @@ namespace {
                  "%s error: %s(0x%.8x)",
                  method,
                  pcsc_stringify_error(result),
-                 result);   
+                 result);
 #else
         snprintf(msg,
                  ERR_MSG_MAX_LEN,
